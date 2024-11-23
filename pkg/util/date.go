9afa3fc044fe4ebbadf5c54555ea0a7be0928489
Enzo <enzo@mfw.local>
2024-11-23 15:20:12 +0700

    fix change to ICT instead of UTC

--- pkg/util/date.go
+++ pkg/util/date.go
@@ -1,6 +1,8 @@
 package util
 
-import "time"
+import (
+	"time"
+)
 
 func Begin(t time.Time) time.Time {
 	year, month, day := t.Date()
@@ -10,4 +12,19 @@ func Begin(t time.Time) time.Time {
 func End(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 23, 59, 59, 0, t.Location())
+}
+
+func IsUTC(t time.Time) bool {
+	return t.Location() == time.UTC
+}
+
+func ConvertToServerTimeZone(t time.Time) (*time.Time, error) {
+	// Load the server's local timezone
+	serverTimeZone, err := time.LoadLocation("Local")
+	if err != nil {
+		return nil, err
+	}
+	// Convert UTC to the server's timezone
+	serverTime := t.In(serverTimeZone)
+	return &serverTime, nil
 }
