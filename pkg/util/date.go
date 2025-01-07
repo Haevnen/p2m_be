f5809f2002038a7f7c6bf11a8042f45736d95ead
enzo <enzo.nguyen@gmail.com>
2025-01-08 00:13:49 +0700

    fix the bug that displays wrong ticket daily

--- pkg/util/date.go
+++ pkg/util/date.go
@@ -6,12 +6,12 @@ import (
 
 func Begin(t time.Time) time.Time {
 	year, month, day := t.Date()
-	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
 
 func End(t time.Time) time.Time {
 	year, month, day := t.Date()
-	return time.Date(year, month, day, 23, 59, 59, 0, t.Location())
+	return time.Date(year, month, day, 23, 59, 59, 0, time.UTC)
 }
 
 func IsUTC(t time.Time) bool {
